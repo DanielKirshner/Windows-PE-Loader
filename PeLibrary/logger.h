e491d86397372f76513c8c17bbfec27197aba2f7
DanielKirshner <d.danielkirshner@gmail.com>
2024-09-26 17:24:37 +0300

    Added logger and dll loader

---
+++ PeLibrary/logger.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <wchar.h>
+
+#ifdef _DEBUG
+	void Logger__debug_log(const wchar_t* format, ...);
+	#define DEBUG_LOG(msg, ...) Logger__debug_log(__FUNCTIONW__ L"@ %d: " msg L"\n", __LINE__, ##__VA_ARGS__);
+
+#else
+	#define DEBUG_LOG(...)
+
+#endif
