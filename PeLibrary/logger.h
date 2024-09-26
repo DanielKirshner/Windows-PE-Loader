1dd20bd0ae9776a97f74d591db3557fd2596ec2b
DanielKirshner <danielkirshner7878@gmail.com>
2024-09-26 17:29:39 +0300

    Aborting in bad behaviors and added some minor changes

--- PeLibrary/logger.h
+++ PeLibrary/logger.h
@@ -3,7 +3,7 @@
 #include <wchar.h>
 
 #ifdef _DEBUG
-	void Logger__debug_log(const wchar_t* format, ...);
+	void Logger__debug_log(const wchar_t* const format, ...);
 	#define DEBUG_LOG(msg, ...) Logger__debug_log(__FUNCTIONW__ L"@ %d: " msg L"\n", __LINE__, ##__VA_ARGS__);
 
 #else
