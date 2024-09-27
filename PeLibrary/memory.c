8bbdf4c9bbc5e30f21fdfc2cfd32eb4c45ea7791
DanielKirshner <danielkirshner7878@gmail.com>
2024-09-27 13:01:53 +0300

    Added memory allocation function that wraps VirtualAlloc call

---
+++ PeLibrary/memory.c
@@ -0,0 +1,42 @@
+#include "memory.h"
+
+#include "logger.h"
+
+bool Memory__allocate(
+	const uint32_t size,
+	void* const requested_base_address,
+	MemoryMap* const memory)
+{
+	void* allocated_ptr = NULL;
+
+	if (memory == NULL)
+	{
+		DEBUG_LOG(L"Bad MemoryMap parameter");
+		return false;
+	}
+	
+	if (requested_base_address == NULL)
+	{
+		DEBUG_LOG(L"Bad requested_base_address parameter");
+		return false;
+	}
+
+	memory->size = 0;
+	memory->start_pointer = NULL;
+
+	allocated_ptr = VirtualAlloc(
+		requested_base_address,
+		size,
+		MEM_COMMIT | MEM_RESERVE,
+		PAGE_EXECUTE_READWRITE);
+
+	if (allocated_ptr == NULL)
+	{
+		DEBUG_LOG(L"Failed to allocate memory with windows error %u", GetLastError());
+		return false;
+	}
+
+	memory->start_pointer = allocated_ptr;
+	memory->size = size;
+	return true;
+}
