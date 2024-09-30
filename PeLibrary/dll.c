d37b1eaf12aea2a5c6e7fa3497e8c5eb29bd30f4
DanielKirshner <danielkirshner7878@gmail.com>
2024-09-30 20:46:09 +0300

    Added more common dll methods

--- PeLibrary/dll.c
+++ PeLibrary/dll.c
@@ -46,3 +46,71 @@ bool Dll__free(Dll* const dll)
 	dll->handle = NULL;
 	return true;
 }
+
+bool Dll__from_handle(const char* const path, Dll* const dll)
+{
+	if (path == NULL || dll == NULL)
+	{
+		DEBUG_LOG(L"Invalid path or dll parameter given!");
+		return false;
+	}
+	
+	dll->handle = NULL;
+	const HMODULE module_handle = GetModuleHandleA(path);
+	
+	if (module_handle == NULL)
+	{
+		DEBUG_LOG(L"Failed to get library from handle with Windows error %u", GetLastError());
+		return false;
+	}
+
+	dll->handle = module_handle;
+	return true;
+}
+
+bool Dll__get_function_by_name(const Dll* const dll, const char* const func_name, FARPROC* const function)
+{
+	if (dll == NULL || func_name == NULL || function == NULL)
+	{
+		DEBUG_LOG(L"One or more parameters given are invalid!");
+		return false;
+	}
+
+	if (dll->handle == NULL)
+	{
+		DEBUG_LOG(L"Dll given is not initialized!");
+		return false;
+	}
+
+	*function = GetProcAddress(dll->handle, func_name);
+
+	if (*function == NULL)
+	{
+		DEBUG_LOG(L"Failed to get address of function %s", func_name);
+		return false;
+	}
+	return true;
+}
+
+bool Dll__get_function_by_ordinal(const Dll* const dll, const uint32_t ordinal, FARPROC* const function)
+{
+	if (dll == NULL || function == NULL)
+	{
+		DEBUG_LOG(L"One or more parameters given are invalid!");
+		return false;
+	}
+
+	if (dll->handle == NULL)
+	{
+		DEBUG_LOG(L"Dll given is not initialized!");
+		return false;
+	}
+	*function = GetProcAddress(dll->handle, MAKEINTRESOURCEA(ordinal));
+
+	if (*function == NULL)
+	{
+		DEBUG_LOG(L"Failed to get address with ordinal %u", ordinal);
+		return false;
+	}
+	return true;
+}
