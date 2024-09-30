a5eb05f68123c7fb74803816db89abf844b7056f
DanielKirshner <d.danielkirshner@gmail.com>
2024-09-30 20:46:09 +0300

    Added more common dll methods

--- PeLibrary/dll.h
+++ PeLibrary/dll.h
@@ -12,3 +12,15 @@ typedef struct _Dll
 bool Dll__load(const char* const path, Dll* const dll);
 
 bool Dll__free(Dll* const dll);
+
+bool Dll__from_handle(const char* const path, Dll* const dll);
+
+bool Dll__get_function_by_name(
+	const Dll* const dll,
+	const char* const func_name,
+	FARPROC* const function);
+
+bool Dll__get_function_by_ordinal(
+	const Dll* const dll,
+	const uint32_t ordinal,
+	FARPROC* const function);
