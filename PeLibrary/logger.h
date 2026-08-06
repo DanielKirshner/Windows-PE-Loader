9aee00fae84da26a059382fa24ffa4d634ee4da2
DanielKirshner <d.danielkirshner@gmail.com>
2026-08-06 14:37:32 +0300

    fix(logger): remove trailing semicolon from DEBUG_LOG macro definition

--- PeLibrary/logger.h
+++ PeLibrary/logger.h
@@ -4,7 +4,7 @@
 
 #ifdef _DEBUG
 	void Logger__debug_log(const wchar_t* const format, ...);
-	#define DEBUG_LOG(msg, ...) Logger__debug_log(__FUNCTIONW__ L"@ %d: " msg L"\n", __LINE__, ##__VA_ARGS__);
+	#define DEBUG_LOG(msg, ...) Logger__debug_log(__FUNCTIONW__ L"@ %d: " msg L"\n", __LINE__, ##__VA_ARGS__)
 
 #else
 	#define DEBUG_LOG(...)
